db73654c0dcd464f85c29ab7680828ac54889670
Max Albrecht <max.albrecht@zhdk.ch>
2018-03-26 19:08:48 +0200

    wip

--- db/migrate/368_remove_obsolete_unlocalized_fields.rb
+++ db/migrate/368_remove_obsolete_unlocalized_fields.rb
@@ -2,6 +2,9 @@ class RemoveObsoleteUnlocalizedFields < ActiveRecord::Migration
 
   # follows up migrations 360-365. values were already migrated, just removed columns.
 
+  # TODO: also move available-/default-language settings to DB.
+  #       then either deal with queries using the old col OR use computed column or something
+
   def up
     execute <<-SQL
 

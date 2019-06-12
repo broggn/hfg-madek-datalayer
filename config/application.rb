e96b8edff9d701ecf442849f8aa8e096547aab5c
Michał Podlecki <m.podlecki@op.pl>
2019-06-12 09:20:53 +0200

    apply 'rails app:update'

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
@@ -8,29 +8,13 @@ Bundler.require(*Rails.groups)
 
 module MadekDatalayer
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
     config.active_record.schema_format = :sql
     config.active_record.timestamped_migrations = false
 
     config.paths['config/initializers'] \
       << Rails.root.join('initializers')
 
-    config.autoload_paths += [
-      Rails.root.join('app', 'queries'),
-      Rails.root.join('app', 'lib'),
-      Rails.root.join('lib')
-    ]
+    config.autoload_paths << Rails.root.join('lib')
 
     if ENV['RAILS_LOG_LEVEL'].present?
       config.log_level = ENV['RAILS_LOG_LEVEL']
@@ -38,6 +22,13 @@ module MadekDatalayer
       config.log_level = :info
     end
 
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+    config.active_record.belongs_to_required_by_default = false
 
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end

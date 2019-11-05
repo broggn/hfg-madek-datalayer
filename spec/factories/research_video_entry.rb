e077ef84466f9448bc1cad443c7412adeebe83e6
Thomas Schank <DrTom@schank.ch>
2019-11-05 13:55:44 +0100

    ... files factory ...

--- spec/factories/research_video_entry.rb
+++ spec/factories/research_video_entry.rb
@@ -7,6 +7,15 @@ FactoryGirl.define do
   factory :research_video_media_file, parent: :media_file do
     association :media_entry, factory: :research_video_entry
     before :create do
+      Madek::System.execute_cmd! \
+        "cp -r #{Madek::Constants::DATALAYER_ROOT_DIR.join(
+          'spec', 'data', 'rv_files', 'originals', '*')} " \
+        " #{Madek::Constants::FILE_STORAGE_DIR} "
+
+      Madek::System.execute_cmd! \
+        "cp -r #{Madek::Constants::DATALAYER_ROOT_DIR.join(
+          'spec', 'data', 'rv_files', 'thumbnails', '*')} " \
+        " #{Madek::Constants::THUMBNAIL_STORAGE_DIR} "
     end
   end
 

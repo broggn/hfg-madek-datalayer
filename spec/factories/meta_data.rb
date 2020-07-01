Madek/madek-datalayer

feat: MetaDatum::MediaEntry

spec/factories/meta_data.rb
@@ -63,6 +63,15 @@ FactoryGirl.define do
      end
    end
 
+   factory :meta_datum_media_entry, class: MetaDatum::OtherMediaEntry do
+     string { Faker::Lorem.sentence }
+     other_media_entry { FactoryGirl.create(:media_entry_with_title) }
+     meta_key do
+       MetaKey.find_by(id: 'test:media_entry') \
+         || FactoryGirl.create(:meta_key_media_entry)
+     end
+   end
+
    factory :meta_datum_keywords, class: MetaDatum::Keywords do
      meta_key do
        MetaKey.find_by(id: 'test:keywords') \

spec/factories/meta_keys.rb
@@ -26,6 +26,11 @@ FactoryGirl.define do
       meta_datum_object_type 'MetaDatum::JSON'
     end
 
+    factory :meta_key_media_entry do
+      id { 'test:media_entry' }
+      meta_datum_object_type 'MetaDatum::OtherMediaEntry'
+    end
+
     factory :meta_key_text_date, class: MetaKey do
       id { 'test:datestring' }
       meta_datum_object_type 'MetaDatum::TextDate'

broggn/hfg-madek-datalayer

spec/factories/meta_data.rb
@@ -63,6 +63,15 @@ FactoryGirl.define do
      end
    end
 
+   factory :meta_datum_media_entry, class: MetaDatum::MediaEntry do
+     string { Faker::Lorem.sentence }
+     other_media_entry { FactoryGirl.create(:media_entry_with_title) }
+     meta_key do
+       MetaKey.find_by(id: 'test:media_entry') \
+         || FactoryGirl.create(:meta_key_media_entry)
+     end
+   end
+
    factory :meta_datum_keywords, class: MetaDatum::Keywords do
      meta_key do
        MetaKey.find_by(id: attributes_for(:meta_key_keywords)[:id]) \

spec/factories/meta_keys.rb
@@ -26,6 +26,11 @@ FactoryGirl.define do
       meta_datum_object_type 'MetaDatum::JSON'
     end
 
+    factory :meta_key_media_entry do
+      id { 'test:media_entry' }
+      meta_datum_object_type 'MetaDatum::MediaEntry'
+    end
+
     factory :meta_key_text_date, class: MetaKey do
       id { 'test:datestring' }
       meta_datum_object_type 'MetaDatum::TextDate'